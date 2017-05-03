62972d22cf3e1403df0e46d7362ff9567404e81c
JacobAhlberg <jacahlberg@hotmail.com>
2017-05-03 11:31:53 +0200

    Added butterknife

--- Android/Scrummerz/da_scrummerz/Android/Scrummerz/app/src/main/java/se/newton/scrummerz/MainActivity.java
+++ Android/Scrummerz/da_scrummerz/Android/Scrummerz/app/src/main/java/se/newton/scrummerz/MainActivity.java
@@ -1,16 +1,13 @@
 package se.newton.scrummerz;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.emailField) EditText emailField;
+    @bindView(R.id.)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
