1ab4dbab029c86eeab9e7db810dbfa47bf4ae155
JacobAhlberg <jacahlberg@hotmail.com>
2017-05-04 11:36:36 +0200

    Removed background and added some comments

--- Android/Scrummerz/app/src/main/java/se/newton/scrummerz/MainActivity.java
+++ Android/Scrummerz/app/src/main/java/se/newton/scrummerz/MainActivity.java
@@ -1,8 +1,9 @@
 package se.newton.scrummerz;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+//        @BindView(R.id.loginBtn)        Button loginBtn;
+//        Button button = (Button) findViewById(R.id.emailField);
+
+//        En annan bra sak med butterknife är att man kan initiera buttons utanför onCreate metoden.
+//        Om man inte kör butterknife så kan man endast deklarera utanför onCreate.
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
     }
 }
