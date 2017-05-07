c9d3174153dbe12e705a7979e38ea96c62b97214
Giovanni Palusa <giovanni@palusa.se>
2017-05-07 11:53:42 +0200

    Two new activities that handles login, welcome screen added

---
+++ Android/Scrummerz/app/src/main/java/se/newton/scrummerz/signedInStart.java
@@ -0,0 +1,13 @@
+package se.newton.scrummerz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signedInStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in_start);
+    }
+}
