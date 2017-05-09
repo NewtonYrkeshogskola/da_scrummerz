af14e2f2af03f96c5d25bc2de0caa48ebbf83e7f
Valerija Grigorjeva <vgrigorjeva@gmail.com>
2017-05-09 15:52:20 +0200

    Get user email and 'global' uid

--- Webb/js/controllers.js
+++ Webb/js/controllers.js
@@ -1,11 +1,22 @@
-(function(){
+var guid;
+
+(function () {
 
     angular
         .module('LoggedIn', ['firebase'])
-        .controller('personCtrl', ["$scope", "$firebaseObject",
-        function($scope, $firebaseObject){
-            var ref = firebase.database().ref();
-            $scope.data = $firebaseObject(ref.child('courses').child('2017'));
-            
-        }]);
+        .factory("Auth", ["$firebaseAuth",
+            function ($firebaseAuth) {
+                return $firebaseAuth();
+            }
+        ])
+        .controller('personCtrl', ["$scope", "$firebaseObject", "Auth",
+            function ($scope, $firebaseObject, Auth) {
+                var ref = firebase.database().ref();
+                $scope.auth = Auth;
+                $scope.data = $firebaseObject(ref.child('courses').child('2017'));
+                $scope.auth.$onAuthStateChanged(function (firebaseUser) {
+                    $scope.firebaseUser = firebaseUser;
+                    guid = firebaseUser.uid;
+                });
+            }]);
 }());
--- Webb/js/loggedin.js
+++ Webb/js/loggedin.js
@@ -1,5 +1,3 @@
-
-// Firebase configuration
 var config = {
     apiKey: "AIzaSyCzIv9ECgSeBxgu_p8oJLDpzoLp4YydvEg",
     authDomain: "scrummerz-2e3f3.firebaseapp.com",
@@ -10,40 +8,30 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 function toggleSignIn() {
     firebase.auth().signOut();
     window.location = "index.html";
 }
-
-var email = "inget@tomt.se";
-var providerData;
-var uid
 
 function initApp() {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             // User is signed in.
             var displayName = user.displayName;
-            email = user.email;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
             var isAnonymous = user.isAnonymous;
-            uid = user.uid;
+            var uid = user.uid;
             var providerData = user.providerData;
-            // ...
+
         } else {
-            // User is signed out.
-            // ...
         }
-
-        return firebase.database().ref('/users/Pupils/' + userId).once('value').then(function (snapshot) {
-            var username = snapshot.val().username;
-            var name = snapshot.val().Name;
-            var pnr = snapshot.val().Pnr;
-
-        });
-
     });
 
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
 
+window.onload = function () {
+    initApp();
+};
