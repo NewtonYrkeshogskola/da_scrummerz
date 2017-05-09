c5112b2c94b7cee02fc44ab19b93bede1e389589
Giovanni Palusa <giovanni@palusa.se>
2017-05-09 11:32:43 +0200

    updated test

--- Webb/js/loggedin.js
+++ Webb/js/loggedin.js
@@ -1,23 +1,61 @@
-function toggleSignIn() {  
-        firebase.auth().signOut();
-        window.location = "index.html";
+// Firebase configuration
+var config = {
+    apiKey: "AIzaSyCzIv9ECgSeBxgu_p8oJLDpzoLp4YydvEg",
+    authDomain: "scrummerz-2e3f3.firebaseapp.com",
+    databaseURL: "https://scrummerz-2e3f3.firebaseio.com",
+    projectId: "scrummerz-2e3f3",
+    storageBucket: "scrummerz-2e3f3.appspot.com",
+    messagingSenderId: "936353564933"
+};
+firebase.initializeApp(config);
+
+function toggleSignIn() {
+    firebase.auth().signOut();
+    window.location = "index.html";
 }
+
+var email;
+var uid;
+var providerData;
+var userId
+
 
 function initApp() {
     firebase.auth().onAuthStateChanged(function (user) {
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
+        if (user) {
+            // User is signed in.
+            displayName = user.displayName;
+            email = user.email;
+            emailVerified = user.emailVerified;
+            photoURL = user.photoURL;
+            isAnonymous = user.isAnonymous;
+            uid = user.uid;
+            userId = user.uid;
+            providerData = user.providerData;
+            // ...
+        } else {
+            // User is signed out.
+            // ...
+        }
 
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+        return firebase.database().ref('/users/Pupils/' + userId).once('value').then(function (snapshot) {
+            var username = snapshot.val().username;
+            var name = snapshot.val().Name;
+            var pnr = snapshot.val().Pnr;
+
+        });
     });
+
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
 
 window.onload = function () {
     initApp();
 }
+
+  var app = angular.module("LoggedIn", []);
+        app.controller("personCtrl", function($scope){
+            $scope.uid = uid;
+            $scope.email = email;
+            
+        });
