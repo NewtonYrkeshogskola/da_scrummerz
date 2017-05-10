96e79432ebef34fdcdf673a79bdf8a2280ed066a
Giovanni Palusa <giovanni@palusa.se>
2017-05-10 09:59:42 +0200

    Cleanup in code, removed firebase reference
    Added gitignore to clean up api-keys from git and added proof of
    concept to admin part

--- Webb/js/controllers.js
+++ Webb/js/controllers.js
@@ -20,5 +20,34 @@ var guid;
                     $scope.user = $firebaseObject(ref.child('users').child('Pupils/' + userId));
                 });
 
-            }]);
+            }])
+        .controller("AdminUserCtrl", ["$scope", "Auth",
+            function ($scope, Auth) {
+                $scope.createUser = function () {
+                    $scope.message = null;
+                    $scope.error = null;
+
+                    // Create a new user
+                    Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+                        .then(function (firebaseUser) {
+                            $scope.message = "Användare skapad med UID: " + firebaseUser.uid;
+                        }).catch(function (error) {
+                            $scope.error = error;
+                        });
+                };
+
+                $scope.deleteUser = function () {
+                    $scope.message = null;
+                    $scope.error = null;
+
+                    // Delete the currently signed-in user
+                    Auth.$deleteUser().then(function () {
+                        $scope.message = "Användare raderad";
+                    }).catch(function (error) {
+                        $scope.error = error;
+                    });
+                };
+            }
+        ]);
+
 }());
--- Webb/js/loggedin.js
+++ Webb/js/loggedin.js
@@ -1,13 +1,3 @@
-var config = {
-    apiKey: "AIzaSyCzIv9ECgSeBxgu_p8oJLDpzoLp4YydvEg",
-    authDomain: "scrummerz-2e3f3.firebaseapp.com",
-    databaseURL: "https://scrummerz-2e3f3.firebaseio.com",
-    projectId: "scrummerz-2e3f3",
-    storageBucket: "scrummerz-2e3f3.appspot.com",
-    messagingSenderId: "936353564933"
-};
-firebase.initializeApp(config);
-
 function toggleSignIn() {
     firebase.auth().signOut();
     window.location = "index.html";
