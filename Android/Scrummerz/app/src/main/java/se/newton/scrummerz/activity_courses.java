715003a6cbc87d4ae153c35926324102eefa2499
Giovanni Palusa <giovanni@palusa.se>
2017-05-16 02:13:24 +0200

    List of courses now work on Android

--- Android/Scrummerz/app/src/main/java/se/newton/scrummerz/activity_courses.java
+++ Android/Scrummerz/app/src/main/java/se/newton/scrummerz/activity_courses.java
@@ -1,8 +1,16 @@
 package se.newton.scrummerz;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -10,31 +18,91 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import se.newton.scrummerz.model.Courses;
 import se.newton.scrummerz.model.Student;
 
 public class activity_courses extends AppCompatActivity {
 
-    String userId, myClass;
+    String userId, myClass, name;
     DatabaseReference dbRef;
+    DatabaseReference classesRef;
+    private RecyclerView allCourses;
+    SharedPreferences studentInfo;
+    private FirebaseRecyclerAdapter<Courses, ItemViewHolder> mAdapter = null;
+
+//    @Override
+//    public void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        myClass = savedInstanceState.getString("class");
+//
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
+        studentInfo = PreferenceManager.getDefaultSharedPreferences(this);
+        myClass = studentInfo.getString("studentClass", "");
+        Log.i("TEST myClass", " " + myClass);
         dbRef = FirebaseDatabase.getInstance().getReference();
-
         getUid();
+        classesRef = dbRef.child("coursesByClass").child(myClass);
         getMyClass(userId);
 
+        allCourses = (RecyclerView) findViewById(R.id.coursesRecyclerView);
+        allCourses.setHasFixedSize(false);
+        allCourses.setLayoutManager(new LinearLayoutManager(this));
+
+    }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("TEST", "classesref: " + classesRef);
+
+        mAdapter = new FirebaseRecyclerAdapter<Courses, ItemViewHolder>(
+                Courses.class,
+                R.layout.item_courses,
+                ItemViewHolder.class,
+                classesRef) {
+
+            @Override
+            protected void populateViewHolder(ItemViewHolder viewHolder, Courses model, int position) {
+                viewHolder.setTitle(model.getName());
+                Log.i("TEST", model.getName());
+                viewHolder.setBody(model.getStatus());
+                Log.i("TEST", model.getStatus());
+            }
+
+        };
+        Log.i("TEST", mAdapter.toString());
+        allCourses.setAdapter(mAdapter);
     }
 
 
-    public void getCourses(){
-        DatabaseReference localRef = dbRef.child("coursesByClass").child(myClass);
+    // Needs to be protected to work on all mobiles
+    @SuppressWarnings("WeakerAccess")
+    protected static class ItemViewHolder extends RecyclerView.ViewHolder {
 
+        View mView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setTitle(String title) {
+            TextView itemTitle = (TextView) mView.findViewById(R.id.Item_title_courseTitle);
+            itemTitle.setText(title);
+        }
+
+        void setBody(String body) {
+            TextView itemBody = (TextView) mView.findViewById(R.id.Item_category);
+            itemBody.setText(body);
+        }
     }
+
 
     public void getUid(){
         try{
@@ -52,7 +120,7 @@ public class activity_courses extends AppCompatActivity {
                 Student student = new Student();
                 student = dataSnapshot.getValue(Student.class);
                 myClass = student.myClass;
-                getCourses();
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -62,6 +130,9 @@ public class activity_courses extends AppCompatActivity {
         });
 
     }
+
+
+
 
 
 }
