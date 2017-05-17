cb17406fdd9e95682f86422ef04705052917ba1a
Valerija Grigorjeva <vgrigorjeva@gmail.com>
2017-05-17 14:04:28 +0200

    grades

--- Webb/js/controllers.js
+++ Webb/js/controllers.js
@@ -17,41 +17,60 @@ app.controller('personCtrl', ["$scope", "$firebaseObject", "$firebaseArray", "Au
         $scope.auth.$onAuthStateChanged(function (firebaseUser) {
             var userId = firebaseUser.uid;
             $scope.firebaseUser = firebaseUser;
-            $scope.user = $firebaseObject(ref.child('users').child('Pupils/' + userId));
-            $scope.userClass = $firebaseObject(ref.child('users').child('Pupils/' + userId));
+            $scope.user = $firebaseObject(ref.child('users').child('students/' + userId).child('details'));
+            // $scope.userClass = $firebaseObject(ref.child('users').child('students/' + userId).child('details'));
             $scope.user.$loaded().then(function () {
                 $scope.data1 = firebase.database().ref().child('courses');
                 $scope.myClass = $scope.user.myClass;
                 var data1 = $scope.data1;
                 var klass = $scope.myClass;
                 $scope.myCourses = $firebaseObject(ref.child('coursesByClass/' + klass));
-                
-                //var query = firebase.database().ref().child('coursesByClass/' + klass);
-/*                query.once('value', snap => console.log(snap.val()));
-                query.once('value', function (snap) {
-                    console.log(snap.val())
-                });
-            $scope.list = $firebaseArray(query);
-                console.log(list);
-                $scope.klassCoursedetails = function () {
-                    query.on('child_added', snap => {
-                        var courseref = data1.child(snap.key);
-                        console.log(courseref.once('value'));
-
+                $scope.myGrades = $firebaseObject(ref.child('users').child('students/' + userId).child('grades').child('courses'));
+                $scope.myGrade = firebase.database().ref().child('users').child('students/' + userId).child('grades').child('courses');
+                $scope.grade = null;
+                $scope.init = function(gra){
+                    $scope.grade = firebase.database().ref().child('users').child('students/' + userId).child('grades').child('courses/' + gra);
+                     $scope.grade.on('value', function (snap) {
+                    snap.forEach(function (Snapshot) {
+                        $scope.kursNamn = Snapshot.key;
+                        $scope.kursBetyg= Snapshot.val();
+                        console.log($scope.kursNamn + ' ' + $scope.kursBetyg);
                     });
+                })
                 }
-                var query444 = $scope.klassCoursedetails($scope.myClass);
-                console.log(query444);
-                var query2 = firebase.database().ref().child('courses');
-                var list2 = $firebaseArray(query2);
-                // console.log(list2);
-                console.log("loaded record:", list2.$getRecord("Courses"));
-                var rec = $scope.myCourses.$id;
-                console.log(rec);
-                return firebase.database().ref('courses').once('value').then(function (snapshot) {
-                    var username = snapshot.val();
-                    console.log(username);
-                });*/
+/*                $scope.myGrade.on('value', function (snap) {
+                    snap.forEach(function (Snapshot) {
+                        $scope.kursNamn = Snapshot.key;
+                        $scope.kursBetyg= Snapshot.val();
+                        console.log($scope.kursNamn + ' ' + $scope.kursBetyg);
+                    });
+                })*/
+                //var query = firebase.database().ref().child('coursesByClass/' + klass);
+                /*                query.once('value', snap => console.log(snap.val()));
+                                query.once('value', function (snap) {
+                                    console.log(snap.val())
+                                });
+                            $scope.list = $firebaseArray(query);
+                                console.log(list);
+                                $scope.klassCoursedetails = function () {
+                                    query.on('child_added', snap => {
+                                        var courseref = data1.child(snap.key);
+                                        console.log(courseref.once('value'));
+                
+                                    });
+                                }
+                                var query444 = $scope.klassCoursedetails($scope.myClass);
+                                console.log(query444);
+                                var query2 = firebase.database().ref().child('courses');
+                                var list2 = $firebaseArray(query2);
+                                // console.log(list2);
+                                console.log("loaded record:", list2.$getRecord("Courses"));
+                                var rec = $scope.myCourses.$id;
+                                console.log(rec);
+                                return firebase.database().ref('courses').once('value').then(function (snapshot) {
+                                    var username = snapshot.val();
+                                    console.log(username);
+                                });*/
             });
         });
     }]);
