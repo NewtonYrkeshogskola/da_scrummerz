b1712b95948c72ec3f699a09ebea189d29991255
Giovanni Palusa <giovanni@palusa.se>
2017-05-25 20:09:31 +0200

    QR-scanner now fully works

--- Android/Scrummerz/app/src/main/java/se/newton/scrummerz/scanner.java
+++ Android/Scrummerz/app/src/main/java/se/newton/scrummerz/scanner.java
@@ -1,8 +1,10 @@
 package se.newton.scrummerz;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -10,6 +12,13 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -20,6 +29,16 @@ public class scanner extends BaseScannerActivity implements ZXingScannerView.Res
     private int REQUEST_CAMERA;
 
 
+    String uid, name, classRef;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference mRoot;
+    DatabaseReference addPresence;
+    Boolean active;
+
+    SharedPreferences studentInfo;
+
+
     // Initiate view and add scanner
     @Override
     public void onCreate(Bundle state) {
@@ -27,6 +46,16 @@ public class scanner extends BaseScannerActivity implements ZXingScannerView.Res
         setContentView(R.layout.activity_scanner);
         mProgressBar = (ProgressBar) findViewById(R.id.scanProgress);
         mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        mRoot = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        uid = currentUser.getUid();
+        studentInfo = PreferenceManager.getDefaultSharedPreferences(this);
+        classRef = studentInfo.getString("studentClass", "");
+        name = studentInfo.getString("studentName", "");
+
 
         ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
 
@@ -76,11 +105,26 @@ public class scanner extends BaseScannerActivity implements ZXingScannerView.Res
     @Override
     public void handleResult(Result scanResult) {
         mProgressBar.setVisibility(ProgressBar.VISIBLE);
+
         String scanResultString = scanResult.getText();
+        addPresence = mRoot.child("coursesByClass/").child(classRef).child(scanResultString);
 
-        Toast.makeText(this, scanResultString, Toast.LENGTH_SHORT).show();
-//        mProgressBar = (ProgressBar) findViewById(R.id.scanProgress);
-//        ScanResult.Result(cleanResultFromScanner, this);
+        addPresence.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                active = dataSnapshot.child("active").getValue().equals(true);
+                if (!active) {
+                    Toast.makeText(scanner.this, "Detta lektionstillfälle finns inte, eller är inte aktivt", Toast.LENGTH_LONG).show();
+                } else {
+                    addPresence.child(uid).setValue(name);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(scanner.this, "Kunde inte hämta från databasen", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
 }
